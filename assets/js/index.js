1a87048fc0165cfa9a9ddb397badb42eda902dda
Elnur <elnurmagerramli@gmail.com>
2020-03-26 02:09:19 +0400

    latest

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// значение кнопок
+
+let keys = document.getElementsByTagName('button');
+
+// function getInput(elements) {
+	// keys.value += elements 
+// };
+
+
+// console.log(getInput());
+
+//  математические операторы
+
+// let mathOperators = ['+', '-', 'x', '÷'];
+
+
+
+// 1-ая переменная
+
+// 2-ая переменная
+
+// функции математических операторов
+// function plus(number1, number2) {
+// 	let result = number1 + number2
+// };
+
+// function minus(number1, number2) {
+// 	let result = number1 - number2
+// };
+
+// что-то еще нужное
+
+// вывод на дисплей с помощью document.querySelectorAll('.screen.innerHTML)
