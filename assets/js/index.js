857881ea10a9c31b7724c06f00f368076944aebc
Elnur <elnurmagerramli@gmail.com>
2020-03-27 02:11:27 +0400

    last

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,11 @@
-// значение кнопок
+// const click = document.getElementById('blabla')
+// function addnumber(){  
+// 	var elem = document.getElementsByClassName('screen');
+// 	elem.innerHTML = 1
 
-let keys = document.getElementsByTagName('button');
+// }
 
-// function getInput(elements) {
-	// keys.value += elements 
-// };
-
-
-// console.log(getInput());
-
-//  математические операторы
-
-// let mathOperators = ['+', '-', 'x', '÷'];
-
-
-
-// 1-ая переменная
-
-// 2-ая переменная
-
-// функции математических операторов
-// function plus(number1, number2) {
-// 	let result = number1 + number2
-// };
-
-// function minus(number1, number2) {
-// 	let result = number1 - number2
-// };
-
-// что-то еще нужное
-
-// вывод на дисплей с помощью document.querySelectorAll('.screen.innerHTML)
+function addnumber() {
+	var x = document.getElementsByClassName('screen').value;
+	x.innerHTML = 1;
+}
