eb7ba80f786c5c335a20db2aca69845a2e03cc87
elnurmagerramli23 <elnurmagerramli@gmail.com>
2020-03-30 13:28:53 +0400

    last

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,72 @@
-// const click = document.getElementById('blabla')
-// function addnumber(){  
-// 	var elem = document.getElementsByClassName('screen');
-// 	elem.innerHTML = 1
+const display = document.querySelector('input.screen');
+let firstNumber = '';
+let secondeNumber = '';
+let operationSign = '';
+let result = 0;
 
-// }
+function clearDisplay() {
+    display.value = '';
+    console.log("Display cleared!");
+}
 
-function addnumber() {
-	var x = document.getElementsByClassName('screen').value;
-	x.innerHTML = 1;
+function insert(value) {
+    display.value += value;
+    console.log("inserted new number in display");
+}
+
+function getSign(sign) {
+    firstNumber = display.value;
+    console.log("remember new number");
+    clearDisplay();
+    
+
+    if(!firstNumber){
+        console.log('input first number')
+        return;
+    }
+    operationSign = sign;
+}
+
+function checkVariables() {
+    secondeNumber = display.value;
+    clearDisplay();
+
+    if(!secondeNumber){
+        return
+    }
+
+    getResult(operationSign);
+    console.log("it works");
+    
+}
+
+const getSum = () => +firstNumber + +secondeNumber;
+const getSubtraction = () => firstNumber - secondeNumber;
+const getMultipication = () => firstNumber * secondeNumber;
+const getDivision = () => firstNumber / secondeNumber;
+// функция ничего не принимает, а switch принимает, как такое может быть?
+//какой из них равно у тебя?
+// равно у меня перемнная в которую после кейса запишется значение подождипопробую позвонить. ок. может проверить сейчас работает кейс или нет?
+function getResult(operationSign) {
+    console.log("get result");
+    
+    switch (operationSign) {
+        case '+':
+           result = getSum();
+            break;
+        case '-':
+            result = getSubtraction();
+            break;
+        case '*':
+            result = getMultipication();
+            break;
+        case '/':
+            result = getDivision();
+            break;
+        default:
+            console.log('default');
+            
+            return;
+    }
+    display.value = result
 }
